cbbaebebd8aba3615093dfbddf8d35b0e2de8fb9
PhilPaquet <143669281+PhilPaquet@users.noreply.github.com>
2025-09-02 20:57:43 -0400

    prep swiper

---
+++ src/scripts/components/Carousel.js
@@ -0,0 +1,11 @@
+import Swiper from 'swiper/bundle';
+
+export default class Carousel {
+  constructor(element) {
+    this.element = element;
+    this.options = {};
+    this.init();
+  }
+
+  init() {}
+}
