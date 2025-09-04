c8be2557d6dcf0121e3b7bb4d17c2e880de0ce75
PhilPaquet <143669281+PhilPaquet@users.noreply.github.com>
2025-09-04 18:42:11 -0400

    swiper fait

--- src/scripts/components/Carousel.js
+++ src/scripts/components/Carousel.js
@@ -3,7 +3,13 @@ import Swiper from 'swiper/bundle';
 export default class Carousel {
   constructor(element) {
     this.element = element;
-    this.options = {};
+    this.options = {
+      pagination: {
+        el: this.element.querySelector('.swiper-pagination'),
+        type: 'bullets',
+        clickable: true,
+      },
+    };
     this.init();
   }
 
